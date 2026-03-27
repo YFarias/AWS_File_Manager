1707a002c41c8c5d377bc7151ffb01d991cb4792
YFarias <yurisfarias@gmail.com>
2026-03-27 09:41:37 -0300

    Refatoracao: - Remover arquivos que sao desneccessarios

--- src/main.py
+++ src/main.py
@@ -4,16 +4,14 @@ from src.api.media_routes import router as media_router
 
 app = FastAPI(title="AWS Manager")
 
-# Configuração de CORS (Permite que o navegador do Front-End se comunique com a API)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Em produção, limite isso para a(s) URL(s) do seu Front-End
+    allow_origins=["*"],  
     allow_credentials=True,
-    allow_methods=["*"],  # Permite opções como GET, POST e o problema atual: OPTIONS
+    allow_methods=["*"],  
     allow_headers=["*"],
 )
 
-# Registro das rotas de forma limpa
 app.include_router(media_router, prefix="/storage")
 
 @app.get("/health")
